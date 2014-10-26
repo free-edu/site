e333e49186a6114335d61175a6f8e0ce39556695
Jason Baker <amnorvend@gmail.com>
2014-10-26 19:33:24 +0000

    Use Openshift ip and port.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+var IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP;
+var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT;
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html');
 });
 
-var server = app.listen(process.env.PORT, process.env.IP);
+var server = app.listen(PORT, IP);
