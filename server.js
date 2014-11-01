ee159bf6f16c7aaa983fa905ce4b7cc3c9e39121
Jason Baker <amnorvend@gmail.com>
2014-11-01 10:13:20 -0700

    Serving static files from local port if not available in environment.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-var IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP;
-var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT;
+var IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '8080';
+
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html');
