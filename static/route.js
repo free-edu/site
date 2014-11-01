acb6a2c6e707ad85ecb449a19dcea2a7dfcb39d0
Jason Baker <amnorvend@gmail.com>
2014-11-01 18:56:45 +0000

    Add handling for root package.

--- static/route.js
+++ static/route.js
@@ -4,12 +4,17 @@
     $routeProvider.when('/', {
       templateUrl: '/static/overview.html'
     });
-    $routeProvider.when('/:category/:page', {
+    $routeProvider.when('/:page', {
       templateUrl: function(args) {
         console.log(args);
+        return '/static/root/' + args['page'] + '.html';
+      }
+    })
+    $routeProvider.when('/:category/:page', {
+      templateUrl: function(args) {
         return '/static/' + args['category'] + '/' + args['page'] + '.html';
-      } 
-    }); 
+      }
+    });
     $locationProvider.html5Mode(true);
   });
 })();
